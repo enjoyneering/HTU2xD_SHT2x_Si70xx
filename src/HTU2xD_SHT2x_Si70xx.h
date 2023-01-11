44aecace74ce7c7785ad36c075e50fd89861de80
enjoyneering <10623649+enjoyneering@users.noreply.github.com>
2023-01-11 16:14:43 -0500

    Update HTU2xD_SHT2x_Si70xx.h

--- src/HTU2xD_SHT2x_Si70xx.h
+++ src/HTU2xD_SHT2x_Si70xx.h
@@ -24,19 +24,19 @@
       measurements must be >= 0.5 seconds apart to keep self-heating below 0.1C
 
    This device uses I2C bus to communicate, specials pins are required to interface
-   Board:                                    SDA              SCL              Level
+   Board                                     SDA              SCL              Level
    Uno, Mini, Pro, ATmega168, ATmega328..... A4               A5               5v
    Mega2560................................. 20               21               5v
    Due, SAM3X8E............................. 20               21               3.3v
    Leonardo, Micro, ATmega32U4.............. 2                3                5v
-   Digistump, Trinket, ATtiny85............. PB0              PB2              5v
+   Digistump, Trinket, Gemma, ATtiny85...... PB0/D0           PB2/D2           3.3v/5v
    Blue Pill*, STM32F103xxxx boards*........ PB9/PB7          PB8/PB6          3.3v/5v
    ESP8266 ESP-01**......................... GPIO0            GPIO2            3.3v/5v
    NodeMCU 1.0**, WeMos D1 Mini**........... GPIO4/D2         GPIO5/D1         3.3v/5v
    ESP32***................................. GPIO21/D21       GPIO22/D22       3.3v
                                              GPIO16/D16       GPIO17/D17       3.3v
                                             *hardware I2C Wire mapped to Wire1 in stm32duino
-                                             see https://github.com/stm32duino/wiki/wiki/API#i2c
+                                             see https://github.com/stm32duino/wiki/wiki/API#I2C
                                            **most boards has 10K..12K pullup-up resistor
                                              on GPIO0/D3, GPIO2/D4/LED & pullup-down on
                                              GPIO15/D8 for flash & boot
@@ -66,7 +66,7 @@
 #include <avr/pgmspace.h>              //for Arduino AVR PROGMEM support
 #elif defined (ESP8266)
 #include <pgmspace.h>                  //for Arduino ESP8266 PROGMEM support
-#elif defined (_VARIANT_ARDUINO_STM32_)
+#elif defined (ARDUINO_ARCH_STM32)
 #include <avr/pgmspace.h>              //for Arduino STM32 PROGMEM support
 #endif
 
@@ -184,15 +184,15 @@ class HTU2xD_SHT2x_SI70xx
    bool     begin(uint8_t sda = SDA, uint8_t scl = SCL, uint32_t speed = HTU2XD_SHT2X_SI70XX_I2C_SPEED_HZ, uint32_t stretch = HTU2XD_SHT2X_SI70XX_I2C_STRETCH_USEC);
   #elif defined (ESP32)
    bool     begin(int32_t sda = SDA, int32_t scl = SCL, uint32_t speed = HTU2XD_SHT2X_SI70XX_I2C_SPEED_HZ, uint32_t stretch = HTU2XD_SHT2X_SI70XX_I2C_STRETCH_USEC);
-  #elif defined (_VARIANT_ARDUINO_STM32_)
+  #elif defined (ARDUINO_ARCH_STM32)
    bool     begin(uint8_t sda = SDA, uint8_t scl = SCL, uint32_t speed = HTU2XD_SHT2X_SI70XX_I2C_SPEED_HZ);
   #else
    bool     begin();
   #endif
 
-   float    readHumidity(HTU2XD_SHT2X_SI70XX_HUMD_OPERATION_MODE_REG = START_HUMD_HOLD_I2C);
+   float    readHumidity(HTU2XD_SHT2X_SI70XX_HUMD_OPERATION_MODE_REG = START_HUMD_HOLD_I2C); //ARDUINO_ARCH_STM32
    float    getCompensatedHumidity(float temperature);
-   float    readTemperature(HTU2XD_SHT2X_SI70XX_TEMP_OPERATION_MODE_REG = START_TEMP_HOLD_I2C);
+   float    readTemperature(HTU2XD_SHT2X_SI70XX_TEMP_OPERATION_MODE_REG = START_TEMP_HOLD_I2C); //ARDUINO_ARCH_STM32
 
    void     setType(HTU2XD_SHT2X_SI70XX_I2C_SENSOR = HTU2xD_SENSOR);
    void     setResolution(HTU2XD_SHT2X_SI70XX_USER_CTRL_RES sensorResolution);
